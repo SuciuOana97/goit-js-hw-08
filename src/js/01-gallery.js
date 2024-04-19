d76d2c47078eb73a3c23757170c10089cad3e6a2
Oanasuciu97 <luciana_bodeaoana@yahoo.com>
2024-04-19 10:43:32 +0200

    task 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Described in documentation;
 
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+function imagePaletteItems() {
+  const list = document.querySelector('.gallery');
+  for (const photo of galleryItems) {
+    //creare tag img + atribute/clasa
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.setAttribute('src', photo.preview);
+    image.setAttribute('alt', photo.description);
+
+    //creare tag li + atribute/clasa
+    const elem = document.createElement('li');
+    elem.classList.add('gallery__item');
+    elem.style.listStyleType = 'none';
+
+    //creare tag a + atribute/clasa
+    const link = document.createElement('a');
+    link.setAttribute('href', photo.original);
+    link.classList.add('gallery__link');
+
+    //aranjare elemente
+    elem.appendChild(link);
+    link.appendChild(image);
+    list.appendChild(elem);
+
+    //Dezactivare comportament redirectionare/download
+    link.addEventListener('click', event => {
+      event.preventDefault();
+    });
+  }
+  //Animatie imagini
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+imagePaletteItems();
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//video element
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+//identify date-seconds from scrolling and logging in localStorage +throttle
+const onPlay = throttle(function (data) {
+  console.log(data);
+  const currentTimeVideo = data.seconds;
+  console.log(currentTimeVideo);
+  localStorage.setItem('videoplayer-current-time', currentTimeVideo);
+}, 1000);
 
+player.on('timeupdate', onPlay);
+//retrieving seconds value from localStorage
+const newStartTime = localStorage.getItem('videoplayer-current-time');
+console.log(newStartTime);
+////resuming video playback from the remaining seconds
